1e676b392b0a35a80231dacfb03f5c5bc9354790
Arnav Gupta <arnav@codingblocks.com>
2018-04-22 12:05:11 +0530

    add concole statement
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const app = require('express')()
 
 const PORT = process.env.PORT || 4444
 
-app.get('/', (req, res) => res.send("HELLO"))
+app.get('/', (req, res) => {
+    console.log("request received")
+    res.send("HELLO")
+})
 
 app.listen(PORT, () => {
     console.log("App started")
